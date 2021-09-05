76a44f35a749446e3b6fac9668bd97a73ab8eede
alexandex <kienva1992@gmail.com>
2021-09-05 11:54:04 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Self_StudyNavigator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Start Task3
+            ////Task1
+            //Console.WriteLine("Введите 2 целых числа");
+
+            //int numberOne = int.Parse(Console.ReadLine());
+            //int numberSecond = int.Parse(Console.ReadLine());
+
+            //Task3.ReturnValue(numberOne, numberSecond);
+
+            ////Task 2
+            //Task3.SecondTask();
+            ////End Task 3
+
+            //Start Task 4
+            Task4.Task2(1, 2, 3, 4);
+            //End Task 4
+        }
+    }
+}
