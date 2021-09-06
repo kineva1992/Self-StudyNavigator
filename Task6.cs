4c965ce43cf48d8ac4a1cef8feb0106e8a5c7ae9
kineva1992 <42785264+kineva1992@users.noreply.github.com>
2021-09-06 16:28:19 +0300

    Update Task6.cs

--- Task6.cs
+++ Task6.cs
@@ -7,7 +7,17 @@ namespace Self_StudyNavigator
 {
     class Task6
     {
+        
         string[] daysOfTheWeek = new string[] {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        var Seasons = new List<string> {"Spring", "Summer", "Autumn", "Winter"};
+        
+        /* Напишите код, в котором:
+        1. Создайте одномерный массив, содержащий
+        наименования дней недели.
+        2. Выведите в консоль значение 5 элемента и длину
+        массива.*/
+        
+        //Start task array
         
         public static void ReturnFiveElementsDaysOfTheWeekArray()
         {
@@ -18,6 +28,18 @@ namespace Self_StudyNavigator
         {
         Console.WriteLine(daysOfTheWeek.length);
         }
-    
+        //End task Array
+        
+        //Start task List
+        
+        public static void PrintSeasonsSummer()
+        {
+        Console.Write(Seasons[1]);
+        }
+        
+        
+        
+        // End task List
+        
     }
 }
