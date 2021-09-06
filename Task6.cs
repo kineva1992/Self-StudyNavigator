28edd06a8293e1b0bbdd359e4be58a66c849dc71
kineva1992 <42785264+kineva1992@users.noreply.github.com>
2021-09-06 12:51:45 +0300

    Update Task6.cs

--- Task6.cs
+++ Task6.cs
@@ -7,6 +7,17 @@ namespace Self_StudyNavigator
 {
     class Task6
     {
+        string[] daysOfTheWeek = new string[] {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        
+        public static void ReturnFiveElements()
+        {
+        Console.WriteLine(daysOfTheWeek[5]);
+        }
+        
+        public static void ReturnLengthArray()
+        {
+        Console.WriteLine();
+        }
     
     }
 }
