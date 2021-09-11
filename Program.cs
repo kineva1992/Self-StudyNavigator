8a577b1c6c57dbdcb173280cf3d86c70329f076c
alexandex <kienva1992@gmail.com>
2021-09-11 14:02:37 +0300

    123

--- Program.cs
+++ Program.cs
@@ -60,6 +60,49 @@ namespace Self_StudyNavigator
             Notification notification = new Notification("them",2);
             //End task11
 
+            //Start task13
+            //State state1 = new State(); // Структура State
+            //State state2 = new State();
+            //state2.x = 1;
+            //state2.y = 2;
+            //state1 = state2;
+            //state2.x = 5; // state1.x=1 по-прежнему
+            //Console.WriteLine(state1.x); // 1
+            //Console.WriteLine(state2.x); // 5
+
+            //Country country1 = new Country(); // Класс Country
+            //Country country2 = new Country();
+            //country2.x = 1;
+            //country2.y = 4;
+            //country1 = country2;
+            //country2.x = 7; // теперь и country1.x = 7, так как обе ссылки и country1 и country2 
+            //                // указывают на один объект в хипе
+            //Console.WriteLine(country1.x); // 7
+            //Console.WriteLine(country2.x); // 7
+
+            //State state1 = new State();
+            //State state2 = new State();
+
+            //state2.country = new Country();
+            //state2.country.x = 5;
+            //state1 = state2;
+            //state2.country.x = 8; // теперь и state1.country.x=8, так как state1.country и state2.country
+            //                      // указывают на один объект в хипе
+            //Console.WriteLine(state1.country.x); // 8
+            //Console.WriteLine(state2.country.x); // 8
+
+
+            //Edn task13
+
+        }
+
+        static void ChangePerson(Persons persons)
+        {
+            //Сработает
+            persons.Name = "Alice";
+            //Сработает только в рамках данного метода.
+            persons = new Persons {Name = "Bill", Age = 45 };
+            Console.WriteLine(persons.Name); // Bill
         }
     }
 }
--- Task13.cs
+++ Task13.cs
@@ -4,7 +4,49 @@ using System.Text;
 
 namespace Self_StudyNavigator
 {
+    struct State
+    {
+        public int x;
+        public int y;
+        public Country country;
+    }
+
+    class Country
+    {
+        public int x;
+        public int y;
+    }
+
+    class Persons
+    {
+        public string Name;
+        public int Age;
+    }
     class Task13
     {
+        /*
+        1. Приведите примеры значимых и ссылочных типов.
+        Значимые типы: byte, sbyte, short, ushort, int, uint, long, ulong, bool, char, enum, struct
+        Ссылочные типы: class, object, interface, delegate, string.
+        2. Будут ли равны значения переменных x и z из примера ниже, если переменные y и z значимого типа?
+        Ссылочного типа?
+        var x = y;
+        y = z; \\ z и y - ранее инициализированные переменные одного типа.
+        3. Что будет выведено в консоль в 7 строке? Можно ли после 6 строки получить доступ к объекту,
+        созданному в 5 строке?
+        1. class Program
+        2. {
+        3. static void Main(string[] args)
+        4. {
+        5. Person p = new Person {name = "Tom"};
+        6. p = new Person {name = "Peter"};
+        7. Console.WriteLine(p.name);
+        9. }
+        9. }
+        10.class Person
+        11.{
+        12. public string name;
+        13.}
+         */
     }
 }
