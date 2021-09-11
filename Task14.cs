83eafc3f01e6d48e120d80c7d8a9c5faf7b4c00e
alexandex <kienva1992@gmail.com>
2021-09-11 23:15:55 +0300

    123

---
+++ Task14.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Self_StudyNavigator
+{
+    public class Gay
+    {
+        public string Name;
+        public int? CountChildred;
+
+        public Gay(string name, int? countChildred)
+        {
+            this.Name = name;
+            this.CountChildred = countChildred;
+        }
+
+        public void GetChildre()
+        {
+            Console.WriteLine(this.CountChildred == null ? $"No data about {this.Name}'s children" : 
+                (this.CountChildred == 0 ? $"{this.Name} has no children" : $"{this.Name} has {this.CountChildred} children"));
+        }
+    }
+   
+    class Task14
+    {
+        public static void CountChildren(string name, int? count)
+        {
+            if (count == null)
+            {
+                Console.WriteLine("{0} Неизвестное количесво детей", name);
+            }
+            else
+            {
+                Console.WriteLine("{0}: Количество детей - {1}", name, count);
+            }
+        }
+    }
+}
